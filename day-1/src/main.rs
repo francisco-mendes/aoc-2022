5d7d987e03d3e7e72178cd8402ddc5aba3fa0069
Francisco Mendes <francisco.a.mendes.98@gmail.com>
2022-12-02 21:20:50 +0000

    Commit day 1 solutions
    Add cargo module
    Add solution
    Add input files
    Add tests

---
+++ day-1/src/main.rs
@@ -0,0 +1,50 @@
+use itertools::Itertools;
+
+const INPUT: &str = include_str!("inputs/given.txt");
+
+fn main() {
+    let mut sums = sum_batched_lined(INPUT).collect_vec();
+
+    let result = sum_n_largest(&mut sums, 1);
+    println!("The amount of calories carried by one elf is {result}.");
+
+    let result = sum_n_largest(&mut sums, 3);
+    println!("The amount of calories carried by three elfs is {result}.");
+}
+
+fn sum_batched_lined(input: &str) -> impl Iterator<Item = u32> + '_ {
+    input
+        .lines()
+        .map(|line: &str| line.parse::<u32>().ok())
+        .batching(|iter| iter.while_some().sum1())
+}
+
+fn sum_n_largest(slice: &mut [u32], n: usize) -> u32
+{
+    let start = slice.len() - n;
+    slice.select_nth_unstable(start);
+    slice[start..].iter().sum()
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn iter_example() {
+        const INPUT: &str = include_str!("inputs/example.txt");
+
+        let result = sum_batched_lined(INPUT);
+
+        assert_eq!(result.collect_vec(), vec![6000, 4000, 11000, 24000, 10000])
+    }
+
+    #[test]
+    fn iter_no_empty_line_at_end() {
+        const INPUT: &str = "1000";
+
+        let result = sum_batched_lined(INPUT);
+
+        assert_eq!(result.collect_vec(), vec![1000])
+    }
+}
